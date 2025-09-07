f1e425c146d27dfa8adbe2c64343daa9fa57d39e
omartmam <omartmam9@gmail.com>
2025-09-07 15:23:06 +0300

    init repo

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const YouTubeClone());
+}
+
+
+class YouTubeClone extends StatelessWidget {
+  const YouTubeClone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      
+    );
+  }
+}
