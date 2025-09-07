55aa7ee4a91e775f8973a9b317013016caed0501
omartmam <omartmam9@gmail.com>
2025-09-07 17:29:29 +0300

    changes on video item

--- lib/Features/home_view/presentation/views/widgets/video_item.dart
+++ lib/Features/home_view/presentation/views/widgets/video_item.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/Core/widgets/custom_text.dart';
 
 class VideoItem extends StatelessWidget {
   const VideoItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.network(
+            'https://i.ytimg.com/vi/7D-6MYpfFmk/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLC8du8OUtV1vhf2FgF3hfrXQIdGnA'),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://yt3.ggpht.com/-B-rvdIH8cZ3vh7vVlTw9nT6h0RYNU2__RsDAMeB_emc_XtnPZiaIRUKH2Gti75u50qXGuNoIAE=s68-c-k-c0x00ffffff-no-rj'),
+              radius: 20,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: [
+                CustomText(title: 'Takkat Chess'),
+                CustomText(title: '400k views')
+              ],
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+      ],
+    );
   }
 }
