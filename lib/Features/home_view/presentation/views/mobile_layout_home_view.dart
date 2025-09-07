74268a9511128521249cc7691073329108383cd8
omartmam <omartmam9@gmail.com>
2025-09-07 15:51:14 +0300

    create mobile home view body

--- lib/Features/home_view/presentation/views/mobile_layout_home_view.dart
+++ lib/Features/home_view/presentation/views/mobile_layout_home_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/Features/home_view/presentation/views/widgets/mobile_home_view_body.dart';
 
 class MobileLayoutHomeView extends StatelessWidget {
   const MobileLayoutHomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body:MobileHomeViewBody() ,
+    );
   }
 }
