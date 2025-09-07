1ba85762bd65430e388ea219b2bd9fa4a7df380e
omartmam <omartmam9@gmail.com>
2025-09-07 16:58:36 +0300

    custom app bar widget

--- lib/Features/home_view/presentation/views/mobile_layout_home_view.dart
+++ lib/Features/home_view/presentation/views/mobile_layout_home_view.dart
@@ -1,28 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/Core/utils/app_styles.dart';
 import 'package:youtube_clone/Core/widgets/custom_text.dart';
 import 'package:youtube_clone/Features/home_view/presentation/views/widgets/mobile_home_view_body.dart';
+import 'package:youtube_clone/Features/home_view/presentation/views/widgets/my_app_bar.dart';
 
-class MobileLayoutHomeView extends StatelessWidget {
+class MobileLayoutHomeView extends StatefulWidget {
   const MobileLayoutHomeView({super.key});
 
   @override
+  State<MobileLayoutHomeView> createState() => _MobileLayoutHomeViewState();
+}
+
+class _MobileLayoutHomeViewState extends State<MobileLayoutHomeView> {
+  TextEditingController controller = TextEditingController();
+  FocusNode focusNode = FocusNode();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Image.network(
-              'https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png',
-              width: 60,
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            CustomText(title: 'YouTube',)
-          ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: MyAppBar(
+              onSubmitted: (value) {},
+              onTap: () {},
+              focusNode: focusNode,
+              controller: controller),
         ),
+        body: MobileHomeViewBody(),
       ),
-      body: MobileHomeViewBody(),
     );
   }
 }
---
+++ lib/Features/home_view/presentation/views/widgets/my_app_bar.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/Core/utils/app_styles.dart';
+import 'package:youtube_clone/Core/widgets/custom_text.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar(
+      {super.key,
+      required this.focusNode,
+      required this.controller,
+      required this.onTap,
+      required this.onSubmitted});
+
+  final TextEditingController controller;
+  final Function()? onTap;
+  final Function(dynamic)? onSubmitted;
+  final FocusNode? focusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.network(
+          'https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png',
+          width: 60,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        CustomText(
+          title: 'YouTube',
+          fontSize: AppStyles.styleSemiBold22(context).fontSize,
+          fontWeight: AppStyles.styleSemiBold22(context).fontWeight,
+        ),
+        SizedBox(
+          width: 15,
+        ),
+        Expanded(
+          child: CupertinoTextField(
+            focusNode: focusNode,
+            onTap: onTap,
+            onSubmitted: onSubmitted,
+            suffix: Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: Icon(
+                Icons.cancel,
+                color: Colors.blue,
+                size: 20,
+              ),
+            ),
+            placeholderStyle:
+                AppStyles.styleRegular16(context).copyWith(color: Colors.black),
+            prefix: Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Icon(
+                CupertinoIcons.search,
+                color: Colors.black,
+                size: 20,
+              ),
+            ),
+            style: AppStyles.styleRegular16(context).copyWith(
+              color: Colors.black,
+            ),
+            controller: controller,
+            placeholder: 'Search...',
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                border: Border.all(color: Colors.white)),
+          ),
+        ),
+      ],
+    );
+  }
+}
