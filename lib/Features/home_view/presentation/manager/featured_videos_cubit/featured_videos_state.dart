5a64f026be9c10ddb74e68206e9b4183c466d5dc
omartmam <omartmam9@gmail.com>
2025-09-07 20:10:02 +0300

    edit on cubit and state

--- lib/Features/home_view/presentation/manager/featured_videos_cubit/featured_videos_state.dart
+++ lib/Features/home_view/presentation/manager/featured_videos_cubit/featured_videos_state.dart
@@ -8,3 +8,17 @@ sealed class FeaturedVideosState extends Equatable {
 }
 
 final class FeaturedVideosInitial extends FeaturedVideosState {}
+
+final class FeaturedVideosLoading extends FeaturedVideosState {}
+
+final class FeaturedVideosSuccess extends FeaturedVideosState {
+  final List<SearchModel> videos;
+
+const  FeaturedVideosSuccess({required this.videos});
+}
+
+final class FeaturedVideosFailure extends FeaturedVideosState {
+  final String errMessage;
+
+  const FeaturedVideosFailure({required this.errMessage});
+}
