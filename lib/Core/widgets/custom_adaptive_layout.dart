8d67aa1ab64fbd65779616b81df85448772b59ff
omartmam <omartmam9@gmail.com>
2025-09-07 15:40:04 +0300

    add size config and custom adaptive layout

---
+++ lib/Core/widgets/custom_adaptive_layout.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/Core/utils/size_config.dart';
+
+
+class CustomAdaptiveLayout extends StatelessWidget {
+  final WidgetBuilder mobileLayout, tabletLayout;
+
+  const CustomAdaptiveLayout({
+    super.key,
+    required this.mobileLayout,
+    required this.tabletLayout,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < SizeConfig.tabletBreakPoint) {
+          return mobileLayout(context);
+        } else {
+          return tabletLayout(context);
+        }
+      },
+    );
+  }
+}
