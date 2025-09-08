f4c247185f1b721d6da70dcff5129e354815f650
omartmam <omartmam9@gmail.com>
2025-09-08 16:34:11 +0300

    create mobile layout video view

--- lib/Features/video_details/presentation/views/video_detail_view.dart
+++ lib/Features/video_details/presentation/views/video_detail_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/Core/widgets/custom_adaptive_layout.dart';
+import 'package:youtube_clone/Features/video_details/presentation/views/mobile_layout_video_view.dart';
 
 class VideoDetailView extends StatelessWidget {
   const VideoDetailView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomAdaptiveLayout(
+        mobileLayout: (context) => MobileLayoutVideoView(),
+        tabletLayout: (context) => SizedBox());
   }
 }
