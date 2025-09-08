2d0be0f6dd4da3e8ab9ea77fd1d85b066fc9e279
omartmam <omartmam9@gmail.com>
2025-09-08 16:40:26 +0300

    create mobile video view body

--- lib/Features/video_details/presentation/views/mobile_layout_video_view.dart
+++ lib/Features/video_details/presentation/views/mobile_layout_video_view.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:youtube_clone/Features/video_details/presentation/views/widgets/mobile_video_view_body.dart';
 
 class MobileLayoutVideoView extends StatelessWidget {
   const MobileLayoutVideoView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+      ),
+      body: MobileVideoViewBody(),
+    );
   }
 }
---
+++ lib/Features/video_details/presentation/views/widgets/mobile_video_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class MobileVideoViewBody extends StatelessWidget {
+  const MobileVideoViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
