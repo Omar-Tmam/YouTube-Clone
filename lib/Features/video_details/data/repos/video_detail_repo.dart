57ef310e841b5653cd4eb22a0ac58d8d26f990bf
omartmam <omartmam9@gmail.com>
2025-09-08 17:43:40 +0300

    finish repo and repo imp

--- lib/Features/video_details/data/repos/video_detail_repo.dart
+++ lib/Features/video_details/data/repos/video_detail_repo.dart
@@ -3,5 +3,5 @@ import 'package:youtube_clone/Core/errors/failures.dart';
 import 'package:youtube_clone/Features/video_details/data/models/video_detail_model/video_detail_model.dart';
 
 abstract class VideoDetailRepo {
-   Future<Either<Failure, VideoDetailModel>> getVideoDetails({required String value});
+   Future<Either<Failure, VideoDetailModel>> getVideoDetails({required String videoId});
 }
