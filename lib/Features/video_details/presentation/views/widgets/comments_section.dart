a14f638590cbfe5ef6ef3c6dca52fc01717160e8
omartmam <omartmam9@gmail.com>
2025-09-10 13:35:52 +0300

    custom widget for comments section

---
+++ lib/Features/video_details/presentation/views/widgets/comments_section.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/Core/utils/app_styles.dart';
+import 'package:youtube_clone/Core/widgets/custom_text.dart';
+
+class CommentsSection extends StatelessWidget {
+  const CommentsSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+      width: double.infinity,
+      decoration: BoxDecoration(
+          color: Colors.grey[900], borderRadius: BorderRadius.circular(8)),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomText(
+                fontWeight: AppStyles.styleSemiBold22(context).fontWeight,
+                fontSize: AppStyles.styleSemiBold18(context).fontSize,
+                title: 'Comments',
+              ),
+              SizedBox(
+                width: 6,
+              ),
+              CustomText(
+                color: Colors.grey,
+                title: '100',
+                fontSize: AppStyles.styleRegular14(context).fontSize,
+              ),
+              Expanded(child: SizedBox()),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Colors.white,
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      WidgetStateProperty.all(Colors.grey[900]),
+                  shadowColor: WidgetStateProperty.all(Colors.white),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 16,
+                backgroundImage: NetworkImage(
+                    'https://yt3.ggpht.com/Bg5wS82KGryRmcsn1YbPThtbXoTmj2XJ9_7LmuE2RF6wbKJBkovfRypbSz6UD3gEu_nHiwGZtQ=s88-c-k-c0x00ffffff-no-rj'),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              CustomText(
+                title: 'can confirm: he never gave us up',
+                fontWeight: AppStyles.styleSemiBold18(context).fontWeight,
+                fontSize: AppStyles.styleRegular14(context).fontSize,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/Features/video_details/presentation/views/widgets/video_player_widget.dart
+++ lib/Features/video_details/presentation/views/widgets/video_player_widget.dart
@@ -7,6 +7,7 @@ import 'package:youtube_clone/Core/widgets/custom_text.dart';
 import 'package:youtube_clone/Features/video_details/data/models/action_button_model.dart';
 import 'package:youtube_clone/Features/video_details/data/models/video_detail_model/video_detail_model.dart';
 import 'package:youtube_clone/Features/video_details/presentation/views/widgets/action_button_row.dart';
+import 'package:youtube_clone/Features/video_details/presentation/views/widgets/comments_section.dart';
 import 'package:youtube_clone/Features/video_details/presentation/views/widgets/video_info_section.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
@@ -71,65 +72,9 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
         SizedBox(
           height: 10,
         ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-          width: double.infinity,
-          decoration: BoxDecoration(
-              color: Colors.grey[900], borderRadius: BorderRadius.circular(8)),
-          child: Column(
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CustomText(
-                    fontWeight: AppStyles.styleSemiBold22(context).fontWeight,
-                    fontSize: AppStyles.styleSemiBold18(context).fontSize,
-                    title: 'Comments',
-                  ),
-                  SizedBox(
-                    width: 6,
-                  ),
-                  CustomText(
-                    color: Colors.grey,
-                    title: '100',
-                    fontSize: AppStyles.styleRegular14(context).fontSize,
-                  ),
-                  Expanded(child: SizedBox()),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down,
-                      color: Colors.white,
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          WidgetStateProperty.all(Colors.grey[900]),
-                      shadowColor: WidgetStateProperty.all(Colors.white),
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 16,
-                    backgroundImage: NetworkImage(
-                        'https://yt3.ggpht.com/Bg5wS82KGryRmcsn1YbPThtbXoTmj2XJ9_7LmuE2RF6wbKJBkovfRypbSz6UD3gEu_nHiwGZtQ=s88-c-k-c0x00ffffff-no-rj'),
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  CustomText(
-                    title: 'can confirm: he never gave us up',
-                    fontWeight: AppStyles.styleSemiBold18(context).fontWeight,
-                    fontSize: AppStyles.styleRegular14(context).fontSize,
-                  )
-                ],
-              )
-            ],
-          ),
-        )
+        CommentsSection()
       ],
     );
   }
 }
+
