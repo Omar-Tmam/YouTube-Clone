f4189c92ecfd5d71066babac202352f5d5ef44d0
omartmam <omartmam9@gmail.com>
2025-09-10 17:57:12 +0300

    finish related cubit

--- lib/Features/video_details/presentation/manager/cubits/related_cubit/related_cubit.dart
+++ lib/Features/video_details/presentation/manager/cubits/related_cubit/related_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:youtube_clone/Features/video_details/data/models/related_model/related_model.dart';
+import 'package:youtube_clone/Features/video_details/data/repos/related_repo/related_repo.dart';
 
 part 'related_state.dart';
 
 class RelatedCubit extends Cubit<RelatedState> {
-  RelatedCubit() : super(RelatedInitial());
+  final RelatedRepo relatedRepo;
+  RelatedCubit(this.relatedRepo) : super(RelatedInitial());
+  Future<void> getRelated(String id) async {
+    emit(RelatedLoading());
+    var data = await relatedRepo.getRelated(id: id);
+    data.fold((error) {
+      emit(RelatedFailure(errMessage: error.errMessage));
+    }, (relatedModel) {
+      emit(RelatedSuccess(relatedModel: relatedModel));
+    });
+  }
 }
