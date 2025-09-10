e8f58cf3d3b377d9d2bd46d48e2294d9bdc2e005
omartmam <omartmam9@gmail.com>
2025-09-10 14:22:57 +0300

    finish comment cubit

--- lib/Features/video_details/presentation/manager/cubits/comment_cubit/comment_cubit.dart
+++ lib/Features/video_details/presentation/manager/cubits/comment_cubit/comment_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:youtube_clone/Features/video_details/data/models/comment_model/comment_model.dart';
+import 'package:youtube_clone/Features/video_details/data/repos/comment_repo/comment_repo.dart';
 
 part 'comment_state.dart';
 
 class CommentCubit extends Cubit<CommentState> {
-  CommentCubit() : super(CommentInitial());
+  CommentCubit(this.commenteRepo) : super(CommentInitial());
+  final CommentRepo commenteRepo;
+  Future<void> getComment(String id) async {
+    emit(CommentLoading());
+    var data = await commenteRepo.getComment(id: id);
+    data.fold((error) {
+      emit(CommentFailure(errMessage: error.errMessage));
+    }, (commentModel) {
+      emit(CommentSucess(commentModel: commentModel));
+    });
+  }
 }
