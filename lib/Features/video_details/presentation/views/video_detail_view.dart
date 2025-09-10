9eb6349893117fd2749141a936513b19e658f3e9
omartmam <omartmam9@gmail.com>
2025-09-10 18:28:59 +0300

    provide related cubit

--- lib/Features/video_details/presentation/views/video_detail_view.dart
+++ lib/Features/video_details/presentation/views/video_detail_view.dart
@@ -3,8 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:youtube_clone/Core/utils/api_service.dart';
 import 'package:youtube_clone/Core/widgets/custom_adaptive_layout.dart';
 import 'package:youtube_clone/Features/video_details/data/repos/comment_repo/comment_repo_implementation.dart';
+import 'package:youtube_clone/Features/video_details/data/repos/related_repo/related_repo_implementation.dart';
 import 'package:youtube_clone/Features/video_details/data/repos/video_detail_repo/video_detail_repo_implementation.dart';
 import 'package:youtube_clone/Features/video_details/presentation/manager/cubits/comment_cubit/comment_cubit.dart';
+import 'package:youtube_clone/Features/video_details/presentation/manager/cubits/related_cubit/related_cubit.dart';
 import 'package:youtube_clone/Features/video_details/presentation/manager/cubits/video_detail_cubit/video_details_cubit.dart';
 import 'package:youtube_clone/Features/video_details/presentation/views/mobile_layout_video_view.dart';
 
@@ -23,7 +25,11 @@ class VideoDetailView extends StatelessWidget {
         BlocProvider(
             create: (context) => CommentCubit(
                 CommentRepoImplementation(apiService: ApiService()))
-              ..getComment(id))
+              ..getComment(id)),
+              BlocProvider(
+            create: (context) => RelatedCubit(
+                RelatedRepoImplementation(apiService: ApiService()))
+              ..getRelated(id))
       ],
       child: CustomAdaptiveLayout(
           mobileLayout: (context) => MobileLayoutVideoView(),
